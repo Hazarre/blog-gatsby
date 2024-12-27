8f8d0f949580fab8bd4a9d5129e3e707773e370b
Hazarre <lihengc6@gmail.com>
2024-12-27 16:41:41 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Hazarre's Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
